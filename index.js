8ed1ddf0614a98abbb76b6a45ea2645c98041892
praise <prayzstanley@gmail.com>
2025-09-16 23:13:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+        var name=("praise");
+        console.log(name)
+        
+        const favouriteNumber=7;
+        var a=5;
+        console.log(a)
+
+        var b=10;
+        console.log(b)
+
+        var a = b;
+        var b = a;
+        var x = 7;
+        var y = 3;
+        var sum = x + y;
+        var age = 15;
+        var futureAge = age + 10;
+        console.log(futureAge)
+
+        var temperature = "Today's temperature is 37C";
+        var firstName = 'praise';
+        var lastName = 'onyedika';
+        var fullName = firstName + lastName;
+        console.log(fullName)
+
+        const length = 6;  
+        const width = 5;
+        const area = length * width
+        console.log(area)
+
+        const daysInWeek = 7;
+        var balance = 100;
+        var deposit = 50;
+        var newBalance = balance + deposit;
+        console.log(newBalance)
+
+        document.getElementById("project").innerHTML= name + "<br>" + favouriteNumber + "<br>" + a + "<br>" + b + "<b>" + b + "<br>" + a + "<br>" + x + "<br>" + y + "<br>" + sum + "<br>" 
+            + age + "<br>" + futureAge + "<br>" + temperature + "<br>" + firstName + "<br>" + lastName + "<br>" + fullName
+            + "<br>" + length + "<br>" + width + "<br>" + area + "<br>" + daysInWeek + "<br>" + balance + "<br>" + deposit
+            + "<br>" + newBalance
