788a92c87c4cf925d478120ab1255a3046dfc67f
praise <prayzstanley@gmail.com>
2025-09-17 13:12:34 +0100

    third commit

--- myScript.js
+++ myScript.js
@@ -1,40 +1,67 @@
-        var name=("praise");
+        let name="praise";
         console.log(name)
         
         const favouriteNumber=7;
-        var a=5;
+        console.log(favouriteNumber)
+
+        var a =5;
         console.log(a)
 
         var b=10;
         console.log(b)
 
-        var a = b;
-        var b = a;
-        var x = 7;
-        var y = 3;
-        var sum = x + y;
-        var age = 15;
-        var futureAge = age + 10;
+        a = 10;
+        console.log(a)
+
+        b = 5;
+        console.log(b)
+
+        let x = 7;
+        console.log(x)
+
+        let y = 3;
+        console.log(y)
+
+        let sum = x + y;
+        console.log(sum)
+
+        let age = 15;
+        console.log(age)
+
+        let futureAge = age + 10;
         console.log(futureAge)
 
-        var temperature = "Today's temperature is 37C";
-        var firstName = 'praise';
-        var lastName = 'onyedika';
-        var fullName = firstName + lastName;
+        let temperature = "Today's temperature is 37C";
+        console.log(temperature)
+
+        let firstName = 'praise';
+        console.log(firstName)
+
+        let lastName = 'onyedika';
+        console.log(lastName)
+
+        let fullName = firstName + lastName;
         console.log(fullName)
 
-        const length = 6;  
+        const length = 6; 
+        console.log(length)
+
         const width = 5;
+        console.log(width)
+
         const area = length * width
         console.log(area)
 
         const daysInWeek = 7;
-        var balance = 100;
-        var deposit = 50;
-        var newBalance = balance + deposit;
+        console.log(daysInWeek)
+
+        let balance = 100;
+        console.log(balance)
+
+        let deposit = 50;
+        console.log(deposit)
+
+        let newBalance = balance + deposit;
         console.log(newBalance)
 
-        document.getElementById("project").innerHTML= name + "<br>" + favouriteNumber + "<br>" + a + "<br>" + b + "<b>" + b + "<br>" + a + "<br>" + x + "<br>" + y + "<br>" + sum + "<br>" 
-            + age + "<br>" + futureAge + "<br>" + temperature + "<br>" + firstName + "<br>" + lastName + "<br>" + fullName
-            + "<br>" + length + "<br>" + width + "<br>" + area + "<br>" + daysInWeek + "<br>" + balance + "<br>" + deposit
-            + "<br>" + newBalance
+        
